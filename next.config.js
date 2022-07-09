ff07ceab83b4a07cc9c34b5a50b2b37064ad1a19
harshavardhan282 <harshavardhanganji2@gmail.com>
2022-07-09 15:45:05 +0530

    first commit

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['cdn.sanity.io'],
+  },
+  swcMinify: true,
+}
+
+module.exports = nextConfig
